9d0122fce7a608997d546353be3ac70511825a62
Jan <jan.hecht@jh-isw.de>
2018-12-02 19:44:09 +0100

    initial DatapointModel

---
+++ WpfMqttClient/Model/DatapointModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMqttClient.Model
+{
+    public class DatapointModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _identifier;
+        public string Identifier
+        {
+            get
+            {
+                return _identifier;
+            }
+            set
+            {
+                if (value == _identifier)
+                {
+                    return;
+                }
+                _identifier = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _value;
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value == _value)
+                {
+                    return;
+                }
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
