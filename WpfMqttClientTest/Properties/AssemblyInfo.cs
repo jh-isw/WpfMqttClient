2f6a7b2ef6c4c592b17cc53df655a447d2868dec
Jan <jan.hecht@jh-isw.de>
2018-12-10 22:57:31 +0100

    add unit tests project

---
+++ WpfMqttClientTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfMqttClientTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfMqttClientTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6524bf1-058f-49a5-9948-c53f8021b11b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfMqttClientTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WpfMqttClient.ViewModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace WpfMqttClientTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static MainViewModel testVM;
+        private static RelayCommand ConnectDisconnectCommand;
+        private static RelayCommand NewDatapointCommand;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext tc)
+        {
+            testVM = new MainViewModel();
+            ConnectDisconnectCommand = testVM.ConnectDisconnectCommand;
+            NewDatapointCommand = testVM.NewDatapointCommand;
+        }
+
+        [TestMethod]
+        public void TestConnectingDisconnectButtonText()
+        {
+            Assert.AreEqual(testVM.ConnectDisconnectButtonText, "Connect");
+            testVM.BrokerUri = "iot.eclipse.org";
+            ConnectDisconnectCommand.Execute(this);
+            Assert.AreEqual(testVM.ConnectDisconnectButtonText, "Disconnect");
+            ConnectDisconnectCommand.Execute(this);
+            Assert.AreEqual(testVM.ConnectDisconnectButtonText, "Connect");
+        }
+
+        //[TestMethod]
+        //public void TestSubscribing()
+        //{
+        //    testVM.BrokerUri = "iot.eclipse.org";
+        //    ConnectDisconnectCommand.Execute(this); //Connect
+        //    testVM.NewDatapointName = "$SYS/broker/uptime";
+        //    testVM.ApplicationMessages = String.Empty;
+        //    NewDatapointCommand.Execute(this);
+        //    StringAssert.Contains(testVM.ApplicationMessages, "seconds");
+        //    ConnectDisconnectCommand.Execute(this); //Disconnect
+        //}
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            testVM.DoCleanup(new MainViewModel.DoCleanupMessage());
+        }
+    }
+}
