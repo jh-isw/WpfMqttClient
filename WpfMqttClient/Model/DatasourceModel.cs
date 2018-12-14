7b3124731bdddfe8349614a5306fc43ef7bd4376
Jan <jan.hecht@jh-isw.de>
2018-12-15 00:23:57 +0100

    add datasource model

---
+++ WpfMqttClient/Model/DatasourceModel.cs
@@ -0,0 +1,128 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMqttClient.Model
+{
+    class DatasourceModel : ModelBase
+    {
+        public DatasourceModel()
+        {
+        }
+
+        public DatasourceModel(bool withTls)
+        {
+            ClientId = Guid.NewGuid().ToString();
+            IMqttClientOptions options;
+            if (withTls)
+            {
+                options = new MqttClientOptionsBuilder()
+                    .WithClientId(ClientId)
+                    .WithTcpServer(BrokerUri)
+                    .WithTls()
+                    .Build();
+            }
+            else
+            {
+                options = new MqttClientOptionsBuilder()
+                    .WithClientId(ClientId)
+                    .WithTcpServer(BrokerUri)
+                    .Build();
+            }
+
+            _managedOptions = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(options)
+                .Build();
+
+            _client = new MqttFactory().CreateManagedMqttClient();
+            _client.Connected += OnConnected;
+            _client.Disconnected += OnDisconnected;
+            _client.ConnectingFailed += OnConnectingFailed;
+        }
+
+        public IManagedMqttClient _client { get; }
+        private ManagedMqttClientOptions _managedOptions;
+
+        private string _clientId;
+        public string ClientId
+        {
+            get
+            {
+                return _clientId;
+            }
+            set
+            {
+                if (value == _clientId)
+                {
+                    return;
+                }
+                _clientId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _brokerUri;
+        public string BrokerUri
+        {
+            get
+            {
+                return _brokerUri;
+            }
+            set
+            {
+                if (value == _brokerUri)
+                {
+                    return;
+                }
+                _brokerUri = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _connectedToBroker;
+        public bool ConnectedToBroker
+        {
+            get
+            {
+                return _connectedToBroker;
+            }
+            set
+            {
+                if (value == _connectedToBroker)
+                {
+                    return;
+                }
+                _connectedToBroker = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async void StartClientAsync()
+        {
+            await _client.StartAsync(_managedOptions);
+        }
+
+        private void OnConnected(object sender, MqttClientConnectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDisconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnConnectingFailed(object sender, MqttManagedProcessFailedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
