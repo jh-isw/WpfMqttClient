d9c0d5bf07304692a3eeb98c73729f57410cca83
Jan <jan.hecht@jh-isw.de>
2018-12-25 10:34:10 +0100

    add selected datasource

--- WpfMqttClient/ViewModel/MainViewModel.cs
+++ WpfMqttClient/ViewModel/MainViewModel.cs
@@ -40,7 +40,6 @@ namespace WpfMqttClient.ViewModel
             {
                 WindowTitle = "Generic MQTT Client using WPF and MQTTnet (Designmode)";
                 BrokerUri = "test.mosquitto.org";
-                ConnectDisconnectButtonText = "Connect";
                 ApplicationMessages = "The" + Environment.NewLine
                     + "quick" + Environment.NewLine
                     + "brown" + Environment.NewLine
@@ -57,10 +56,6 @@ namespace WpfMqttClient.ViewModel
                 WindowTitle = "Generic MQTT Client using WPF and MQTTnet";
 
                 DispatcherHelper.Initialize();
-
-                ConnectDisconnectButtonText = "Connect";
-                ConnectDisconnectCommand = new RelayCommand(OnConnectDisconnectExecuted, OnConnectDisconnectCanExecute);
-                ConnectDisconnectReturnKeyCommand = new RelayCommand(OnConnectDisconnectExecuted, null);
 
                 ConnectCommand = new RelayCommand(OnConnectExecuted, OnConnectCanExecute);
                 ConnectReturnKeyCommand = new RelayCommand(OnConnectExecuted, null);
@@ -80,8 +75,13 @@ namespace WpfMqttClient.ViewModel
 
                 Messenger.Default.Register<DoCleanupMessage>(this, DoCleanup);
 
-                Datasources = new ObservableCollection<DatasourceModel>();
+                var dsList = new List<DatasourceModel>();
+                Datasources = new ObservableCollection<DatasourceModel>(dsList);
                 DatasourcesView = CollectionViewSource.GetDefaultView(Datasources) as ListCollectionView;
+                DatasourcesView.CurrentChanged += (s, e) =>
+                {
+                    RaisePropertyChanged(() => SelectedDatasourceModel); Console.WriteLine("current changed");
+                };
                 
                 var dpList = new List<DatapointModel>();
                 dpList.Add(new DatapointModel
@@ -94,7 +94,7 @@ namespace WpfMqttClient.ViewModel
                 {
                     item.PropertyChanged += DatapointsPropertyChanged;
                 }
-                //Datapoints.CollectionChanged += UpdateDatapointList;
+
                 DatapointsView = CollectionViewSource.GetDefaultView(Datapoints) as ListCollectionView;
                 DatapointsView.CurrentChanged += (s, e) =>
                 {
@@ -105,37 +105,10 @@ namespace WpfMqttClient.ViewModel
 
         private void DatapointsPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            //DatapointsView.Refresh();
             DispatcherHelper.RunAsync(() => DatapointsView.Refresh());
         }
 
-        //private void UpdateDatapointList(object sender, NotifyCollectionChangedEventArgs e)
-        //{
-        //    foreach (DatapointModel var in e.NewItems)
-        //    {
-        //        Console.WriteLine(var.ToString());
-        //    }
-        //}
-
         public string WindowTitle { get; private set; }
-
-        private string _connectDisconnectButtonText;
-        public string ConnectDisconnectButtonText
-        {
-            get
-            {
-                return _connectDisconnectButtonText;
-            }
-            set
-            {
-                if (value == _connectDisconnectButtonText)
-                {
-                    return;
-                }
-                _connectDisconnectButtonText = value;
-                RaisePropertyChanged();
-            }
-        }
 
         private string _brokerUri;
         public string BrokerUri
@@ -245,8 +218,6 @@ namespace WpfMqttClient.ViewModel
             }
         }
 
-        private IManagedMqttClient Client;
-
         private ObservableCollection<DatapointModel> Datapoints { get; }
         public DatapointModel SelectedDatapointModel
         {
@@ -260,12 +231,18 @@ namespace WpfMqttClient.ViewModel
         public ICollectionView DatapointsView { get; }
 
         private ObservableCollection<DatasourceModel> Datasources;
+        public DatasourceModel SelectedDatasourceModel
+        {
+            get => DatasourcesView.CurrentItem as DatasourceModel;
+            set
+            {
+                DatasourcesView.MoveCurrentTo(value);
+                RaisePropertyChanged();
+            }
+        }
         public ICollectionView DatasourcesView { get; }
 
         #region Commands
-        public RelayCommand ConnectDisconnectCommand { get; private set; }
-        public static RelayCommand ConnectDisconnectReturnKeyCommand { get; private set; }
-
         public RelayCommand ConnectCommand { get; private set; }
         public static RelayCommand ConnectReturnKeyCommand { get; private set; }
 
@@ -277,61 +254,10 @@ namespace WpfMqttClient.ViewModel
         public RelayCommand ClearOutputBoxContent { get; private set; }
 
         public RelayCommand WithTlsCommand { get; private set; }
-
-        private async void OnConnectDisconnectExecuted()
-        {
-            if ((Client == null) || (Client != null && !Client.IsStarted)) // kein Client bisher erzeugt oder angehalten
-            {
-                try
-                {
-                    IMqttClientOptions options;
-                    if (WithTls)
-                    {
-                        options = new MqttClientOptionsBuilder()
-                            .WithClientId(ClientId)
-                            .WithTcpServer(BrokerUri)
-                            .WithTls()
-                            .Build();
-                    }
-                    else
-                    {
-                        options = new MqttClientOptionsBuilder()
-                            .WithClientId(ClientId)
-                            .WithTcpServer(BrokerUri)
-                            .Build();
-                    }
-
-                    var managedOptions = new ManagedMqttClientOptionsBuilder()
-                        .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
-                        .WithClientOptions(options)
-                        .Build();
-                    Client = new MqttFactory().CreateManagedMqttClient();
-                    Client.ApplicationMessageReceived += OnMessageReceived;
-                    Client.Connected += OnConnected;
-                    Client.Disconnected += OnDisconnected;
-                    Client.ConnectingFailed += OnConnectingFailed;
-                    await Client.StartAsync(managedOptions);
-                    ConnectDisconnectButtonText = "Disconnect";
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
-            else // Client ist erzeugt
-            {
-                await Client.StopAsync();
-                ConnectDisconnectButtonText = "Connect";
-            }
-        }
-
-        private bool OnConnectDisconnectCanExecute()
-        {
-            return true;
-        }
         
         private void OnConnectExecuted()
         {
+            Datasources.Add(new DatasourceModel(BrokerUri, WithTls));
             BrokerUri = "";
         }
 
@@ -340,9 +266,9 @@ namespace WpfMqttClient.ViewModel
             return true;
         }
 
-        private async void OnNewDatapointExecuted()
+        private void OnNewDatapointExecuted()
         {
-            await Client.SubscribeAsync(new TopicFilterBuilder().WithTopic(/*"$SYS/broker/uptime"*/NewDatapointName).Build());
+            //await Client.SubscribeAsync(new TopicFilterBuilder().WithTopic(/*"$SYS/broker/uptime"*/NewDatapointName).Build());
             NewDatapointName = "";
         }
 
@@ -369,43 +295,13 @@ namespace WpfMqttClient.ViewModel
         }
         #endregion
 
-        private void OnConnectingFailed(object sender, MqttManagedProcessFailedEventArgs e)
-        {
-            ConnectedToBroker = false;
-#if DEBUG
-            Console.WriteLine("OnConnectingFailed called with e: " + e.ToString());
-#endif
-            ApplicationMessages += "Connection failed\n";
-            ConnectDisconnectButtonText = "Stop trying";
-        }
-
-        private void OnConnected(object sender, MqttClientConnectedEventArgs e)
-        {
-            ApplicationMessages += "Connection success\n";
-            ConnectedToBroker = true;
-        }
-
-        private void OnDisconnected(object sender, MqttClientDisconnectedEventArgs e)
-        {
-            ConnectedToBroker = false;
-        }
-
-        private void OnMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
-        {
-            var message = System.Text.Encoding.Default.GetString(e.ApplicationMessage.Payload);
-            Console.WriteLine(message);
-            ApplicationMessages += message + "\n";
-            // Liste nach Topic durchlaufen und Wert updaten
-            Datapoints[0].Value = message;
-        }
-
         public void DoCleanup(DoCleanupMessage obj)
         {
-            if (Client != null && Client.IsConnected)
-            {
-                Client.StopAsync();
-                Client.Dispose();
-            }
+            //if (Client != null && Client.IsConnected)
+            //{
+            //    Client.StopAsync();
+            //    Client.Dispose();
+            //}
         }
 
         public class DoCleanupMessage
